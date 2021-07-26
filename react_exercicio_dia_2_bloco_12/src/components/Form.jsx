c89372ee15e78a255f924164c3cab8c355d8905e
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-26 18:57:24 -0300

    comeca a criar o componente Form

--- react_exercicio_dia_2_bloco_12/src/components/Form.jsx
+++ react_exercicio_dia_2_bloco_12/src/components/Form.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+	constructor(){
+		super();
+		this.state={
+			name:'',
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange({ target }) {
+		const { name, value } = target;
+		this.setState({
+			[name]: value,
+		});
+	}
+
 	render() {
 		return (
-			
+			<fieldset>
+				<label htmlFor="input-name">
+					Nome:
+					<input
+					name="name"
+					id="input-name"
+					maxLength="40"
+					required="true"
+					value={this.handleChange}			
+					/>
+				</label>
+			</fieldset>
 		);
 	}
 }
